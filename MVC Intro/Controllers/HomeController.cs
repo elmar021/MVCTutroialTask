5c6a79de755e292ddbc9e1ce00c0c89382294395
elmar021 <bd7ml6y3l@code.edu.az>
2024-01-27 00:06:01 +0400

    task added

---
+++ MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ContentResult ContentResult()
+        {
+            return Content("Hello BB206");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult actionResult()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
